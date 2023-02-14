399e6e4bff4f1bbb7c6acd1a3ac75b03c705fb15
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-14 15:57:34 +0900

    feat : 로그인이 한 사용자 (쿠키 사용)

---
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,41 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     *
+     * @return null 로그인 실패
+     */
+    public Member login(String loginId, String password){
+        Optional<Member> findMemberOptional = memberRepository.findByLoginId(loginId);
+
+        Member member = findMemberOptional.get();
+
+        if(member.getPassword().equals(password)){
+            return member;
+        }else {
+            return null;
+        }
+
+        /* Optional 객체라 .stream을 안해도 인식을 한다.
+
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+
+*/
+
+
+    }
+}
